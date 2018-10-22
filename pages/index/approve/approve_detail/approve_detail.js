4f98165d68ecff6adcac518c582895c3b8ea8b59
Coward <949942046@qq.com>
2018-10-22 11:17:45 +0800

    我完成了审批详情页的js文件

--- pages/index/approve/approve_detail/approve_detail.js
+++ pages/index/approve/approve_detail/approve_detail.js
@@ -1,104 +1,133 @@
-// pages/index/require/require_detail/require_detail.js
+var app = getApp();
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        staff_name: '刘兴丽',
-        staff_state: '1',
-        items: [{
-            xueyuan: '计算机学院',
-            staff_name: '刘兴丽',
-            date: 'XX-XX XX:XX',
-            collage: '软件工程',
-            before: '第一周 星期一 第一节',
-            after: '第二周 星期一 第一节'
-        }],
-        forms: [{
-            formname: '安安安',
-            formstate: '0',
-            formdate: 'XX-XX XX:XX'
-        },
-        {
-            formname: '车向前',
-            formstate: '0',
-            formdate: 'XX-XX XX:XX'
-        }, {
-            formname: '张丽媛',
-            formstate: '1',
-            formdate: 'XX-XX XX:XX'
-        }]
+        form: {},
+        date: [],
+        user:{}
     },
+    agree(e){
+        var form=this.data.form;
+        console.log(form);
+        wx.request({
+            url: app.globalData.config + "relay",
+            method: "POST",
+            data: {
+                form_flow: form.form_flow,
+                form_flow_sign: form.form_flow_sign,
+                form_id: form.form_id,
+                from_userid: this.data.user.staff_id,
+                update_time: ''
+            },
+            success(res) {
+                if (res.data.status === 200) {
+                    setTimeout(function () {
+                        wx.hideLoading();
+                        wx.showToast({
+                            title: res.data.message,
+                        })
+                        setTimeout(function () {
+                            wx.switchTab({
+                                url: '../../index',
+                            })
+                        }, 1000);
 
+                    }, 1000)
+                } else if (res.data.status === 400) {
+                    wx.hideLoading();
+                    wx.showToast({
+                        title: res.data.message,
+                        mask: true
+                    })
+                } else if (res.data.status === 401) {
+                    wx.hideLoading();
+                    wx.showToast({
+                        title: res.data.message,
+                        mask: true,
+                        image: '/static/ico/fail.png'
+                    })
+                }
+            }
+        })
+    },
+    refuse(e){
+
+    },
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
-        var _this = this;
-        //判断审批状态
-        var data = _this.data.forms;
-        for (var item in data) {
-            if (data[item].formstate == 0) {
-                data[item].formstate = '审批完成'
-            } else if (data[item].formstate == 1) {
-                data[item].formstate = '审批中'
-            } else {
-                data[item].formstate = '审批失败'
-            }
+    onLoad: function(options) {
+        var user=wx.getStorageSync("user");
+        var form = JSON.parse(options.form)
+        this.setData({
+            form: form
+        })
+        var date_before = [];
+        var date_after = [];
+        var date_before = form.form_before_adjust.split(',');
+        var date_after = form.form_later_adjust.split(',');
+        var date = [];
+        for (var i = 0; i < date_before.length; i++) {
+            var arr = [];
+            arr[0] = date_before[i];
+            arr[1] = date_after[i];
+            date.push(arr);
         }
-        //判断这张表单的审批状态
-        var state = _this.data.staff_state;
-        (state == 0) ? _this.setData({ staff_state: '审批完成' }) : _this.setData({ staff_state: '审批中' });
-        _this.setData({ forms: data })
+        this.setData({
+            date:date,
+            user:user
+        })
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
+    onReady: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
+    onShow: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function () {
+    onHide: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function () {
+    onUnload: function() {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
+    onPullDownRefresh: function() {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
+    onReachBottom: function() {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
+    onShareAppMessage: function() {
 
     }
 })
